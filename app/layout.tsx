c7997f9477cbd4f4dc016b4ef5923178daf9b601
Alphalance247 <al.moshood017@gmail.com>
2024-01-05 21:39:54 +0100

    new commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,30 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Header from '../components/Header';
-import Footer from '../components/Footer';
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
-
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'JayTee Ojo Foundation',
-  description: 'JayTee Ojo Foundation',
-}
+  title: "JayTee Ojo Foundation",
+  description: "JayTee Ojo Foundation",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-      <div>
-      <Header />
-        {children}
-      <Footer />
-      </div>
+        <div>
+          <Header />
+          {children}
+          <Footer />
+        </div>
       </body>
     </html>
-  )
+  );
 }
