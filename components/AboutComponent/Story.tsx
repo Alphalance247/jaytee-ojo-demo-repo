424fd79242cb3a57c960b372611cf4b6c3f8938d
Alphalance247 <al.moshood017@gmail.com>
2024-01-15 02:51:33 +0100

    About page design

---
+++ components/AboutComponent/Story.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Button from "../Button";
+
+const Story = () => {
+  return (
+    <div className=" p-24 flex gap-20">
+      <Image
+        width={596}
+        height={410}
+        src="/assets/About/Volunteer.jpg"
+        alt="Volunteer"
+      />
+      <div className=" flex flex-col justify-start items-start gap-8">
+        <h1 className=" text-[#262626] font-RobotoSlab text-3xl font-bold">
+          Our Story
+        </h1>
+        <p className=" text-[#595959] text-lg font-Roboto font-normal">
+          We have, among other projects, donated solar streetlights to light up
+          communities at night, we have donated borehole water fountains, we
+          have sponsored several medical interventions, food banks, sports
+          intervention as well as farmer empowerment and local farming
+          interventions.
+        </p>
+        <p className="text-[#595959] text-lg font-Roboto font-normal">
+          For all the intervention projects and works mentioned above and many
+          more besides, we are prouder of the investments we make into the
+          future via educational interventions from donating books to students,
+          to multiple scholarship programmes that cover school fees, various
+          examination registration fees, accommodation, and projects support.
+        </p>
+        <Button text="LEARN MORE" />
+      </div>
+    </div>
+  );
+};
+
+export default Story;
--- pages/AboutUs/About.tsx
+++ pages/AboutUs/About.tsx
@@ -1,7 +1,16 @@
 import Layout from "@/app/layout";
+import Hero from "@/components/AboutComponent/Hero";
+import Story from "@/components/AboutComponent/Story";
+import Mission from "@/components/AboutComponent/Mission";
 
 const About = () => {
-  return <Layout>{}</Layout>;
+  return (
+    <Layout>
+      <Hero />
+      <Story />
+      <Mission />
+    </Layout>
+  );
 };
 
 export default About;
