cbcfac6037c79943ae290964202c7a7a14a55bfe
Abdul Azeez <al.moshood017@gmail.com>
2025-11-24 12:17:57 +0100

    Refactor code structure for improved readability and maintainability

---
+++ pages/env/env.local.ts
@@ -0,0 +1,3 @@
+const environment = {
+  baseUrl: "https://site-api.alluvium.net",
+};
--- pages/layout.tsx
+++ pages/layout.tsx
@@ -1,10 +1,12 @@
 "use client";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import ToastNotification from "@/components/common/toastNotification";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
+      <ToastNotification />
       <Header />
       {children}
       <Footer />
